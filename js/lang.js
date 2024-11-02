4aedd92a74676d2d637d0625946bd0eac86eb003
belal-188 <97530168+belal-188@users.noreply.github.com>
2024-11-02 05:05:01 +0300

    Delete js/lang.js

--- js/lang.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".current-lang").onclick = function() {
-    document.querySelector(".lang-dropdown").style.display = "block";
-};
-
-window.addEventListener("click", function(event) {
-    if (!event.target.closest('.switch-lang')) {
-        document.querySelector(".lang-dropdown").style.display = "none";
-    }
-});
